a872bf4389c796210dfe65960d718344c32b2b35
Patizitaz8 <112134929+Patizitaz8@users.noreply.github.com>
2022-09-16 23:23:20 -0400

    Add files via upload

---
+++ Chapter 5/Calculate change/Calculate change/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculate_change
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            const int quarter = 25;
+            const int dime = 10;
+            const int nickel = 5;
+            const int peny = 1;
+            int inputAmount = Convert.ToInt32(txtInput.Text);
+
+            if (inputAmount >= quarter)
+            {
+                txtQuarters.Text = (inputAmount / quarter).ToString();
+                inputAmount = inputAmount % quarter;
+            }
+
+            if (inputAmount >= dime)
+            {
+                txtDimes.Text = (inputAmount / dime).ToString();
+                inputAmount = inputAmount % dime;
+            }
+
+            if (inputAmount >= nickel)
+            {
+
+                txtNickels.Text = (inputAmount / nickel).ToString();
+
+                inputAmount = inputAmount % nickel;
+            }
+
+
+            if (inputAmount >= peny)
+            {
+                txtPennies.Text = (inputAmount / peny).ToString();
+
+                inputAmount = inputAmount % peny;
+            }
+        }
+
+    }
+}
---
+++ Chapter 6/Simple Calculator/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Simple_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = Convert.ToDecimal(txtValue1.Text);
+            decimal operand2 = Convert.ToDecimal(txtValue2.Text);
+            String Operator= txtOperator.Text;
+
+
+            decimal result = CalculateResult(operand1, Operator, operand2);
+            result = Math.Round(result, 4);
+            txtresult.Text = result.ToString();
+            txtValue1.Focus();
+        }
+
+        private decimal CalculateResult(decimal operand1, String Operator, decimal operand2)
+        {
+            decimal result = 0;
+
+            if (txtOperator.Text == "+")
+            
+                {
+                result = (operand1 + operand2);
+                }
+
+            else if (txtOperator.Text == "-")
+
+                 {
+                result = (operand1 - operand2);
+                 }
+
+            else if (txtOperator.Text == "*")
+
+                {
+                result = (operand1 * operand2);
+                }
+
+            else if (txtOperator.Text == "/")
+
+                 {
+                result = (operand1 / operand2);
+                 }
+
+            return result;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtValue1_TextChanged(object sender, EventArgs e)
+        {
+            txtresult.Text = "";
+        }
+
+        private void txtValue2_TextChanged(object sender, EventArgs e)
+        {
+            txtresult.Text = "";
+        }
+    }
+}
+
